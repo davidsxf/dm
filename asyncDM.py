a29df5795941e20cb58f0defe2465d9818303784
lll <bupt_ljl@163.com>
2016-11-23 22:10:24 +0800

    spider success

---
+++ asyncDM.py
@@ -0,0 +1,158 @@
+#coding= utf-8
+
+import asyncio
+import time
+import aiohttp
+import traceback
+import re
+#from bs4 import BeautifulSoup
+#import requests
+from lxml import html as HTML
+import traceback
+import json
+import random
+from page import page
+
+#board_site = {'新闻': 'news', '娱乐': 'ent', '体育': 'sports', '财经': 'finance', '科技': 'tech', '游戏': 'games', '教育': 'edu', '房产': 'house', '汽车': 'auto'}
+proxy = ['124.88.67.17:82', '1.82.216.135:80', '124.88.67.32:843',
+         '1.82.216.134:80', '58.217.195.141:80', '122.72.18.160:80', '223.67.136.218:80', '120.52.21.132:8082', 
+         '120.52.72.58:80', '120.52.72.56:80', '124.88.67.20:843', '124.88.67.54:843',
+         '124.88.67.63:80']
+         #'183.250.179.29:80', 
+N = len(proxy) - 1
+
+
+class task_url(object):
+    @asyncio.coroutine
+    def print_page(self, url, res):
+        #print ('start')
+        header = {
+            'Accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'h-CN,zh;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'roll.news.qq.com',
+            'Referer': 'http://roll.news.qq.com/index.htm?site=news&mod=1&date=2016-11-07&cata=',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
+    }
+        try:
+            #p = 'http://' + proxy[random.randint(0, N)]
+            #p = 'http://124.88.67.17:82'
+            #conn = aiohttp.ProxyConnector(p)
+            response = yield from aiohttp.request('GET', url, headers=header)#, connector=conn)
+            body =  yield from response.read()
+            response.close()
+            #print (url)
+        except:
+            traceback.print_exc()
+            #print (url)
+            return
+        #print (1)
+        try:
+            data = json.loads(body.decode('gbk'))
+        
+            if data['data']:
+                print (url)
+                html = HTML.fromstring(data['data']['article_info'])
+                url_li = html.xpath('//li')
+                #print (len(url_li))
+                #print (type(utl_type))
+                for li in url_li:
+                    try:
+                        #url_type = li.xpath('span[@class="t-tit"]/text()')[0]
+                    #except:
+                    #    print (li.xpath('text()')[0]) 
+                    #    url_type = 'CBA or NBA'
+                    #print (str(url_type))
+                        #if str(url_type):# != '[ 图片 ]' and str(url_type) != '[ &nbsp;彩票&nbsp; ]':
+                    #    raise Exception
+                    #if str(url_type) == '[国内]':
+                    #if str(url_type) == '[国内]' or '[社会]':
+                        #raise Exception
+                        url_article = li.xpath('a/@href')
+                        #print (url_article)
+                        res.extend(url_article)
+                    except:
+                        traceback.print_exc()
+                        pass
+            else:
+                pass
+        except:
+            traceback.print_exc()
+            return
+        #print (str(url_article[0]))
+        '''
+        for u in url_article:
+            conn = pymysql.connect(host='localhost', user='root', passwd='zxcvbnm123', port=3306, db='test', charset='UTF8')
+            cur = conn.cursor()
+            name = str(u)
+            sql = 'insert into user(num, name, url) values ("2", "2", "1")'#+name+', '+name+', '+str(u)+')'
+            cur.execute(sql)
+            sql = 'select * from user'
+            cur.execute(sql)
+            print (cur.fetchall())
+            cur.close()
+            conn.close() 
+        '''
+
+        
+    
+    def result(self, start, stop):
+        loop = asyncio.get_event_loop()
+        urls = []
+        result = []
+        site = 'games'
+        for mon in range(start, stop):
+            if mon<10:
+                mon_time = '0' + str(mon)
+            else:
+                mon_time = str(mon)
+            for i in range(1, 31):
+                if i<10:
+                    i_time = '0' + str(i)
+                else:
+                    i_time = str(i)
+                for j in range(1, 5):
+                    url1 = 'http://roll.news.qq.com/interface/roll.php?0&cata=&site='+site+'&date=2016-'+mon_time+'-'+i_time+'&page='+str(j)+'&mode=1&of=json'
+                    url2 = 'http://roll.news.qq.com/interface/roll.php?0&cata=&site='+site+'&date=2015-'+mon_time+'-'+i_time+'&page='+str(j)+'&mode=1&of=json'
+                    url3 = 'http://roll.news.qq.com/interface/roll.php?0&cata=&site='+site+'&date=2014-'+mon_time+'-'+i_time+'&page='+str(j)+'&mode=1&of=json'
+                    url4 = 'http://roll.news.qq.com/interface/roll.php?0&cata=&site='+site+'&date=2013-'+mon_time+'-'+i_time+'&page='+str(j)+'&mode=1&of=json'
+                    url5 = 'http://roll.news.qq.com/interface/roll.php?0&cata=&site='+site+'&date=2012-'+mon_time+'-'+i_time+'&page='+str(j)+'&mode=1&of=json'
+                    url6 = 'http://roll.news.qq.com/interface/roll.php?0&cata=&site='+site+'&date=2011-'+mon_time+'-'+i_time+'&page='+str(j)+'&mode=1&of=json'
+                    url7 = 'http://roll.news.qq.com/interface/roll.php?0&cata=&site='+site+'&date=2010-'+mon_time+'-'+i_time+'&page='+str(j)+'&mode=1&of=json'
+                    urls.append(url1)
+                    urls.append(url2)
+                    urls.append(url3)
+                    urls.append(url4)
+                    urls.append(url5)
+                    urls.append(url6)
+                    urls.append(url7)
+        #for i in range(2, 3):
+        #    urls.append('http://sports.qq.com/l/isocce/xijia/laliganews_'+str(i)+'.htm')
+        #u = ['http://roll.news.qq.com/interface/roll.php?0&cata=&site='+'sports'+'&date=2016-'+'03'+'-'+'21'+'&page='+'1'+'&mode=1&of=json']
+        tasks = [self.print_page(url, result) for url in urls]
+        #print (type(tasks), len(tasks))
+        length = len(tasks)//400
+        if not length:
+            loop.run_until_complete(asyncio.wait(tasks))
+        for t in range(length):
+            task = tasks[t*400:t*400+400]
+            loop.run_until_complete(asyncio.wait(task))
+        #loop.close()
+        return result
+
+
+if __name__ == '__main__':
+    print (time.ctime())
+    end = []
+    t = task_url()
+    res = t.result(1, 13)
+
+    print (len(res))
+    print (len(list(set(res))))
+
+    
+    pag = page()
+    pag.return_page(res, end)
+    print (time.ctime())
---
+++ old/page.py
@@ -0,0 +1,144 @@
+import asyncio
+import json
+import aiohttp
+from lxml import html as HTML
+#import requests
+import random
+import traceback
+import pymysql
+
+header = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'news.qq.com',
+            'Referer': 'http://roll.news.qq.com/',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+        }
+    
+
+us = ['http://ent.qq.com/a/20161111/026252.htm', 'http://ent.qq.com/a/20161111/026658.htm', 
+      'http://ent.qq.com/a/20161111/026207.htm', 'http://news.qq.com/a/20161118/001449.htm',
+      'http://news.qq.com/a/20161118/001363.htm', 'http://news.qq.com/a/20161118/000633.htm',
+      'http://news.qq.com/a/20161117/043487.htm', 'http://news.qq.com/a/20161117/043348.htm',
+      'http://news.qq.com/a/20161117/042390.htm', 'http://news.qq.com/a/20161118/008272.htm',
+      'http://news.qq.com/a/20161118/009324.htm', 'http://news.qq.com/a/20161118/004480.htm']
+
+proxy = ['124.88.67.17:82', '1.82.216.135:80', '124.88.67.32:843',
+         '1.82.216.134:80', '58.217.195.141:80', '122.72.18.160:80', '223.67.136.218:80', '120.52.21.132:8082', 
+         '120.52.72.58:80', '120.52.72.56:80', '124.88.67.20:843', '124.88.67.54:843',
+         '124.88.67.63:80']
+
+#'183.250.179.29:80', 
+#'118.180.15.151:8102'  124.88.67.17:82',      , '221.226.82.130:8998'          120.52.72.56:80
+N = len(proxy) - 1
+
+class page(object):
+    @asyncio.coroutine
+    def get_page(self, url, cnt):
+        #print ('start')
+        #print ('next')
+        article = ''
+        art_text = ''
+
+        try:
+            #p = 'http://' + proxy[random.randint(0, N)]
+            p = 'http://' + proxy[1] 
+            #print (p)
+            conn = aiohttp.ProxyConnector(proxy=p)
+            request = yield from aiohttp.request('GET', url, connector=conn)
+            page = yield from request.read()
+            request.close()
+            #print (len(cnt))
+            pro = p
+        except:
+            traceback.print_exc()
+            #print (len(cnt), p)
+            return 0
+        try:
+            html = HTML.fromstring(page.decode('gbk'))
+        except:
+            return 0
+            #open('test.txt', 'w').write(url+' '+'gbk'+'\n')
+        try:
+            article_all = html.get_element_by_id("Cnt-Main-Article-QQ") 
+            #print (article_all, url)
+            #hd = article_all.xpath('div[@class="hd"]/h1[1]/text()')[0]
+            #bd = article_all.xpath('div[@class="bd"]')[0]
+            #bd = html.find_class('bd')[0]
+            #text = bd.xpath('//div[@class="Cnt-Main-Article-QQ"]/p[@style="TEXT-INDENT: 2em"]/text()')
+            text = article_all.xpath('./p[@style="TEXT-INDENT: 2em"]/text()')
+            
+            text1 = article_all.xpath('./p[@style="TEXT-INDENT:2em"]/text()')
+            for p in text:
+                art_text += str(p)
+            for p in text1:
+                art_text += str(p)
+            #print (art_text)
+            #conn =  pymysql.connect(host='localhost', user='root', passwd='zxcvbnm123', port=3306, db='dm', charset='utf8')
+            #cur  = conn.cursor()
+            #sql = "insert into news_in values ("++",,)"
+            
+            if len(art_text)>100:# and '彩票' not in art_text:
+                if not len(cnt)%400:
+                    print (len(cnt))
+                art_text = art_text.replace("‘", "")
+                art_text = art_text.replace("“", "")
+                #print (art_text, url)
+                cnt.append(url)
+                #print (len(cnt), pro)
+                
+                conn =  pymysql.connect(host='localhost', user='root', passwd='zxcvbnm123', port=3306, db='dm', charset='utf8')
+                cur  = conn.cursor()
+                sql = "insert into games values ("+str(len(cnt)-1)+",'"+url+"','"+art_text+"')"
+                cur.execute(sql)
+                cur.close()
+                conn.commit()
+                conn.close()
+                
+                #print (len(cnt))
+                #print ('write!')
+            else:
+                pass
+                #print (url)
+        except IndexError:
+            #traceback.print_exc()
+            #print (url, 'no bd')
+            return
+        except:
+            traceback.print_exc()
+            return 0
+            #open('test.txt', 'w').write(url+'\n')
+        #print (len(text))
+        #print (text)
+        #print (art_text)
+
+    def return_page(self, urls, tmp):
+        print (len(urls))
+        #print (urls)
+        loop = asyncio.get_event_loop()
+        tasks = [self.get_page(url, tmp) for url in urls]
+        length = len(tasks)//100
+        if length == 0:
+            loop.run_until_complete(asyncio.wait(tasks))
+        else:
+            for t in range(length):
+                try:
+                    task = tasks[t*100: t*100+100]
+                except:
+                    task = tasks[t*100:]
+                try:
+                    loop.run_until_complete(asyncio.wait(task))
+                except:
+                    pass
+        loop.close()
+        print (len(tmp))
+
+if __name__ == '__main__':
+    task = page()
+    l = []
+    task.return_page(us, l)
+
