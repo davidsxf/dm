5eedbad9825873db2fee5f7629ffa215e7aacf98
lll <bupt_ljl@163.com>
2016-11-30 08:02:49 +0800

    dm

--- dm.py
+++ dm.py
@@ -17,13 +17,13 @@ f.close()
 class_all = ['news_in', 'news_cn', 'ent', 'sports', 'finance', 'tech', 'games', 'edu', 'house', 'auto']
 cls_name = {'news_in':0, 'news_cn':1, 'ent':2, 'sports':3, 'finance':4, 'tech':5, 'games':6, 'edu':7, 'house':8, 'auto':9}
 
-def featureSel(cls, w_list, w_num, cls_w, all_w):
+def featureSel(cls, w_list, cls_w, all_w):
     pynlpir.open()
     word = []
     
     conn = pymysql.connect(host='localhost', user='root', passwd='zxcvbnm123', port=3306, db='dm', charset='utf8')
     cur = conn.cursor()
-    sql = 'select * from ' + cls + ' where num<22000'
+    sql = 'select * from ' + cls + ' where num<20000'
     cur.execute(sql)
     text = cur.fetchall()
     cur.close()
@@ -36,8 +36,6 @@ def featureSel(cls, w_list, w_num, cls_w, all_w):
     word_cls = []
     for te in text:
         
-        
-
         try:
             t = pynlpir.segment(te[2])
             #w_list.append(t)
@@ -49,7 +47,6 @@ def featureSel(cls, w_list, w_num, cls_w, all_w):
                     words.append(w[0])
                     if w[0] not in cls_words:
                         cls_words.append(w[0])
-                    
                     if w[0] not in res:
                         res[w[0]] = 0
                     res[w[0]] += 1
@@ -81,48 +78,73 @@ def featureSel(cls, w_list, w_num, cls_w, all_w):
 def dm():
     
     word_all = []
-    word_num = []
     cls_w = []
     all_w = []
     for cls in class_all:
-        featureSel(cls, word_all, word_num, cls_w, all_w)
-    n = 0
+        featureSel(cls, word_all, cls_w, all_w)
+    N = 0
     for i in range(len(class_all)):
-        n += len(word_all[i])
-    N = n
+        N += len(word_all[i])
     print (N)
     corpus = []
     cls_corpus = []
     fea = []
+    res_all = []
     for i in range(len(class_all)):
         res = {}
-        
         for w in cls_w[i]:
-            num_else = 0
-            len_else = 0
+            cnt = 0
+            for k in all_w[i]:
+                if w in k:
+                    cnt += 1
+            if cnt:
+                res[w] = cnt
+        res_all.append(res)
+    all_w = []
+
+    for i in range(len(class_all)):
+        res = {}
+        num_else = N - len(word_all[i])
+        for w in cls_w[i]:
             a = 0
             b = 0
             c = 0      
             d = 0
+            
+            try:
+                a = res_all[i][w]
+            except:
+                pass
+            for i_no in range(i):
+                try:
+                    b += res_all[i_no][w]
+                except:
+                    pass
+            for i_no in range(i+1, len(class_all)):
+                try:
+                    b+= res_all[i_no][w]
+                except:
+                    pass
+            '''
             for k in all_w[i]:
                 if w in k:
-                    a += k[w]
+                    a += 1
 
             for i_no in range(i):
-                num_else += len(word_all[i_no])
+                #num_else += len(word_all[i_no])
                 for k in all_w[i_no]:
                     if w in k:
-                        len_else += k[w]
+                        b += 1
                     
             for i_no in range(i+1, len(class_all)):
-                num_else += len(word_all[i_no])
+                #num_else += len(word_all[i_no])
                 for k in all_w[i_no]:
                     if w in k:
-                        len_else += k[w]
-                    
-            c = len(cls_w[i]) - a
+                        b += 1
+            '''
+            c = len(word_all[i]) - a
             d = num_else - b
-
+            #print (a,b,c,d)
             z1 = a*d - b*c
             z2 = (z1 * z1 * N * 1.0) /((a+c)*(a+b)*(b+d)*(c+d))
             res[w] = z2
@@ -130,7 +152,7 @@ def dm():
         for re in sorted(res.items(), key=lambda x:x[1], reverse=True)[:500]:
             if re[0] not in fea:
                 fea.append(re[0])
-        print (class_all[i]+'col done!')
+        print (class_all[i]+' col done!')
     print (len(fea))
     for i in range(len(class_all)):
         for val in word_all[i]:
@@ -145,6 +167,10 @@ def dm():
     #print (len(corpus))
         print (class_all[i]+' is done!')
     return corpus, cls_corpus
+    word_all = []
+    cls_w = []
+    all_w = []
+    res_all = []
     #print (len(cls_w), len(all_w))
     #return word_all, word_num, cls_w, all_w
     #print (word_all[0])
@@ -155,6 +181,25 @@ def dm():
 if __name__ == '__main__':
     
     corpus, cls = dm()
+    print (len(corpus), len(cls))
+    '''
+    conn = pymysql.connect(host='localhost', user='root', passwd='zxcvbnm123', port=3306, db='dm', charset='utf8')
+    #print (type(corpus[0]))
+    for k in range(len(cls)):
+        cur = conn.cursor()
+        sql = "insert into tfidf values ('"+cls[k]+"','"+corpus[k]+"',"+str(k)+")"
+        #print (sql)
+        try:
+            cur.execute(sql)
+        except:
+            #traceback.print_exc()
+            pass
+        cur.close()
+    conn.commit()
+    conn.close()
+    '''
+
+    
     vectorizer=CountVectorizer()#该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频  
     transformer=TfidfTransformer()#该类会统计每个词语的tf-idf权值  
     tfidf=transformer.fit_transform(vectorizer.fit_transform(corpus))#第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵  
@@ -163,29 +208,26 @@ if __name__ == '__main__':
     weight=tfidf.toarray()#将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重  
     y = []
     x = []
-    f = open('set.txt', 'w')
+    #f = open('set.txt', 'w')
     for i in range(len(weight)):#打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重  
         #print (u"-------这里输出第",i,u"类文本的词语tf-idf权重------")  
         wdic = {}
-        f.write(str(cls_name[cls[i]])+' ')
+        #f.write(str(cls_name[cls[i]])+' ')
         for j in range(len(word)):
             
             wdic[j] = weight[i][j]
-            f.write(str(j)+':'+str(weight[i][j])+' ')
+            #f.write(str(j)+':'+str(weight[i][j])+' ')
             #print (word[j],weight[i][j])
-        f.write('\n')
+        #f.write('\n')
         #dic = sorted(wdic.items(), key=lambda x:x[1], reverse=True)
         #print (len(wdic))
         
         y.append(cls_name[cls[i]]) 
         x.append(wdic)
-    f.close()
+    #f.close()
     #print (x)
     #print (y, x)
     #print (y[0], len(x[0]))
-    #print (y[-1], len(x[-1]))
-    #os.chdir('C://Users/lll/Desktop/big data/dm/libsvm-3.21/python')
-    #print (os.getcwd())
     sys.path.append('C://Users/lll/Desktop/big data/dm/libsvm-3.21/python')
     from svmutil import *
     m = svm_train(y[::2], x[::2])
@@ -195,35 +237,3 @@ if __name__ == '__main__':
 
 
 
-
-
-def second_handle():
-#函数用于进行第二层处理，获取TF和IDF；
-#参数说明，database_name_list:需要处理的数据库列表
-    database_name_list=["caijing","gongyi","jiankang","jiaoyu","junshi","keji","lvyou","nvxing","wenshi","yule"];
-    every_file_list={};#建立存放单词的字典，统计每个词出现的次数,即包含该单词的文章篇数
-    f=0;
-    for database_name in database_name_list:
-        print("database_name:"+database_name)
-        get_data(every_file_list,database_name);
-    print(len(every_file_list));
-    for database_name in database_name_list:
-        file_num=1;#表编号;
-        while file_num<20000:#对每个数据库中的所有文件进行处理，但找IDF值只在每个数据库的前10000篇找
-            words_IDF_dic={};
-            table_name = database_name + str(file_num);  # 将要处理的表名
-            get_words(database_name,table_name,words_IDF_dic);
-            #print(words_IDF_dic);
-
-            get_IDF(every_file_list,words_IDF_dic);
-            for key in words_IDF_dic:
-                TF=float(words_IDF_dic[key][0])/float(words_IDF_dic[key][1]);
-                TF_IDF=TF*math.log(100000/float(words_IDF_dic[key][2]+1));
-                words_IDF_dic[key][3]=TF_IDF;
-            if f==0:
-                print(words_IDF_dic);
-                f+=1;
-                #input("请输入：");
-            print(database_name+":"+table_name);
-            file_num+=1;
-            update_table_with_IDF(database_name,table_name,words_IDF_dic);
